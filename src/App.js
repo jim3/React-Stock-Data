fb3041100b5a4de86ec5d6cce9e27c369ab3cb58
jim3 <j3.casey@protonmail.com>
2023-05-13 14:43:25 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -2,41 +2,53 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-async function getWeatherData() {
-    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-    const zipCode = "90210";
-    const unit = "imperial";
-    const URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=${unit}`;
+// https://www.alphavantage.co/documentation/#intraday Alphavantage API
+// This API returns raw (as-traded) daily open/high/low/close/volume values, daily adjusted close values, 
+// and historical split/dividend events of the global equity specified, covering 20+ years of historical data.
+async function getStockData() {
+    const apiKey = process.env.API_KEY;
+    const func = "TIME_SERIES_DAILY_ADJUSTED";
+    const symbol = "MSFT";
+    const baseURL = `https://www.alphavantage.co/query?`;
+    const endPoint = `function=${func}&symbol=${symbol}&apikey=${apiKey}`;
+    const URL = baseURL + endPoint;
+    console.log(URL);
+    
     const response = await fetch(URL, {
         method: "GET",
     });
     return response.json();
 }
 
-function Weather() {
-    // Create a state variable to hold the weather data
+function Stocks() {
+    // Create a state variable to hold the stock data (Alphavantage API)
     const [data, setData] = useState(null);
 
-    // Fetch the weather data when the component mounts and store it in state (the setData function)
+    // Fetch weather data
     useEffect(() => {
         async function fetchData() {
-            const weatherData = await getWeatherData();
-            setData(weatherData); // set the state of the weather data object
+            const stockData = await getStockData();
+            setData(stockData); // set the state of the weather data object
         }
         fetchData();
     }, []);
 
     if (data) {
         // Create a list of weather components to display
-        const weatherComponents = [
-            <h2>Weather in {data.name}</h2>,
-            <p>Temperature: {data.main.temp} &deg;F</p>,
-            <p>Feels like: {data.main.feels_like} &deg;F</p>,
-            <p>Humidity: {data.main.humidity} %</p>,
-            <p>Wind Speed: {data.wind.speed} mph</p>,
+        const stockComponents = [
+            <div key="1">
+                <h1>Stock Data</h1>
+                <p>Stock Symbol: {data["Meta Data"]["2. Symbol"]}</p>
+                <p>Stock Information: {data["Meta Data"]["1. Information"]}</p>
+                <p>Stock Last Refreshed: {data["Meta Data"]["3. Last Refreshed"]}</p>
+                <p>Stock Output Size: {data["Meta Data"]["4. Output Size"]}</p>
+                <p>Stock Time Zone: {data["Meta Data"]["5. Time Zone"]}</p>
+            </div>,
+
+        
         ];
         // Return the weather components
-        return <div>{weatherComponents}</div>;
+        return <div>{stockComponents}</div>;
     } else {
         return <div>Loading...</div>;
     }
@@ -45,7 +57,9 @@ function Weather() {
 export default function MyApp() {
     return (
         <div>
-            <Weather />
+            <Stocks />
         </div>
     );
 }
+
+// const URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=${unit}`;
